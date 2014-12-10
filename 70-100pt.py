fe5928d00b66b363bbf2c0dfc1213fee3ac7a512
Gabriel-Diaz-Dos-Santos <pokemondude214@gmail.com>
2014-12-10 08:54:43 -0600

    Lab 16 = 90 point

--- 70-100pt.py
+++ 70-100pt.py
@@ -50,22 +50,51 @@ class myApp(object):
         global rocket1Fired
         x1,y1,x2,y2 = drawpad.coords(enemy)
         px1,py1,px2,py2 = drawpad.coords(player)
-
+        #rocketsx1, rocketsy1, rocketsx2, rocketsy2 = drawpad.coords(rockets)
+        if rocket1Fired == True:
+            drawpad.move(rocket1,0,-5)
         if x2 > 800:
             direction = - 5
         elif x1 < 0:
             direction = 5
         drawpad.move(enemy, direction, 0)
         drawpad.after(5,self.animate)
+        
+        global direction
+    # Get the x and y co-ordinates of the circle
+        x1, y1, x2, y2 = drawpad.coords(player)
+        if x2 > drawpad.winfo_width(): 
+                direction = - 4
+        elif x1 < 3:
+            direction = 4
+    #Move our oval object by the value of direction
+            drawpad.move(player,direction,0)
 
     def key(self,event):
         global player
         global rocket1Fired
+        x1,y1,x2,y2 = drawpad.coords(player)
         if event.char == "w":
-            drawpad.move(player,0,-4)
-            drawpad.move(rocket1,0,-4)
-            
-    
+            if y1 > 0:
+                drawpad.move(player,0,-4)
+                drawpad.move(rocket1,0,-4)
+        if event.char == "a":
+            if x1 > 0:
+                drawpad.move(player,-4,0)
+                drawpad.move(rocket1,0,-4) 
+        if event.char == "s": 
+            if y2 < 600:
+                drawpad.move(player,0,4)
+                drawpad.move(rocket1,0,-4)  
+        if event.char == "d": 
+            if x2 <800:
+                drawpad.move(player,4,0)
+                drawpad.move(rocket1,0,-4)          
+        if event.char == " ":
+            self.rockets = self.rockets -1
+            self.rocketsTxt.configure(text=self.rockets)
+            rocket1Fired = True
+           
     def collisionDetect(self, rocket):
         rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
 app = myApp(root)
